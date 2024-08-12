c1a3fa91f440ec4a6437960d2e36f08995ceaadc
jveer634 <jaysparx9@gmail.com>
2024-08-12 19:29:43 +0530

    chore: code reformat

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,17 @@
-import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
-import { decodeSuiPrivateKey } from "@mysten/sui/cryptography";
-import { Network, TurbosSdk } from "turbos-clmm-sdk";
-
-import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
-
-import "dotenv/config";
 import { MIST_PER_SUI } from "@mysten/sui/utils";
-
-const sdk = new TurbosSdk(Network.devnet);
+import { Fee, getFeeType, Mode, setUp } from "./utils";
 
 const tokenA = "0x2::sui::SUI";
 const tokenB =
 	"0xf427e3a0f0817f5425c091d323941c4be96e272cc895787bd6f910c2ffd66a90::usdc::USDC";
 
-const fee =
-	"0x800d280a8c03db824964d49e76fc8504c10c2d63b81c962af287e1157f15c920::fee3000bps::FEE3000BPS";
-
 const main = async () => {
-	const client = new SuiClient({
-		url: getFullnodeUrl("testnet"),
-	});
-
-	const senderKeypair = decodeSuiPrivateKey(process.env.KEY as string);
-
-	const signer = Ed25519Keypair.fromSecretKey(senderKeypair.secretKey);
+	const { sdk, client, signer } = setUp(Mode.Development);
 	const receiver = signer.toSuiAddress();
-
 	const pools = await sdk.pool.getPools();
+
+    
+	const fee = getFeeType(Mode.Development, Fee["3000BPS"]);
 
 	const pool = pools.filter(
 		(p) => p.types.indexOf(tokenB) != -1 && p.types.indexOf(tokenA) != -1
---
+++ src/utils.ts
@@ -0,0 +1,55 @@
+import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
+import { decodeSuiPrivateKey } from "@mysten/sui/cryptography";
+import { Network, TurbosSdk } from "turbos-clmm-sdk";
+
+import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
+
+import "dotenv/config";
+
+export enum Mode {
+	"Production",
+	"Development",
+}
+
+export enum Fee {
+	"100BPS",
+	"500BPS",
+	"3000BPS",
+	"10000BPS",
+}
+
+// NOTE: Here I am only considering ED25519 Private Key, change it accordingly to your requirement
+export const setUp = (mode: Mode) => {
+	let sdk: TurbosSdk;
+	let client: SuiClient;
+
+	if (mode == Mode.Development) {
+		sdk = new TurbosSdk(Network.devnet);
+		client = new SuiClient({
+			url: getFullnodeUrl("testnet"),
+		});
+	} else {
+		sdk = new TurbosSdk(Network.mainnet);
+		client = new SuiClient({
+			url: getFullnodeUrl("mainnet"),
+		});
+	}
+
+	const senderKeypair = decodeSuiPrivateKey(process.env.KEY as string);
+	const signer = Ed25519Keypair.fromSecretKey(senderKeypair.secretKey);
+
+	return { sdk, client, signer };
+};
+
+// Helper function to get the Fee type
+export const getFeeType = (mode: Mode, fee: Fee) => {
+	if (mode == Mode.Development) {
+		return `0x800d280a8c03db824964d49e76fc8504c10c2d63b81c962af287e1157f15c920::${fee
+			.toString()
+			.toLowerCase()}::FEE${fee.toString().toUpperCase()}`;
+	} else {
+		return `0x84d1ad43e95e9833670fcdb2f2d9fb7618fe1827e3908f2c2bb842f3dccb80af::${fee
+			.toString()
+			.toLowerCase()}::FEE${fee.toString().toUpperCase()}`;
+	}
+};
